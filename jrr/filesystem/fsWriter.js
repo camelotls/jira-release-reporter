f9b4ac0d56ab106dc8e474a99fcfc02aea9a4ba7
konstantinos perperidis <konstantinos.perperidis@camelotls.com>
2021-05-24 16:39:06 +0300

    [DIG32917]: try to set the mount to read only

--- jrr/filesystem/fsWriter.js
+++ jrr/filesystem/fsWriter.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 const write = (filename, content) => {
-  fs.mkdirSync('/jira-release-reporter/report');
   fs.writeFile(filename, content, (err) => {
     err ? console.log(err) : null;
   });
