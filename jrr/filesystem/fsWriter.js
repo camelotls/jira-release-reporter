046f002c04294f9a1221df2910cb0c005b4c6f4d
konstantinos perperidis <konstantinos.perperidis@camelotls.com>
2021-05-24 16:32:56 +0300

    [DIG32917]: changed report output dir.

--- jrr/filesystem/fsWriter.js
+++ jrr/filesystem/fsWriter.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const write = (filename, content) => {
+  fs.mkdirSync('/jira-release-reporter/report');
   fs.writeFile(filename, content, (err) => {
     err ? console.log(err) : null;
   });
